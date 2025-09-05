77dc8b6753f4947dfc41ab3e2c2503ad44063713
Arnau <arnau@ersilia.io>
2025-09-05 13:14:00 +0200

    fixing input reader

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,5 +1,6 @@
 # imports
 import os
+import csv
 import sys
 import json
 
@@ -13,6 +14,19 @@ from chebifier.cli_adapted import predict
 # parse arguments
 input_file = sys.argv[1]
 output_file = sys.argv[2]
+tmp_file = output_file.replace(".csv", '_tmp.csv')
+
+# read smiles and create tmp file
+with open(input_file, "r") as f:
+    reader = csv.reader(f)
+    next(reader)  # skip header
+    smiles_list = [r[0] for r in reader]
+
+with open(tmp_file, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["input"])
+    for s in smiles_list:
+        writer.writerow([s])
 
 # change working directory before running the model
 os.chdir(os.path.join(root, "python-chebifier"))
@@ -21,14 +35,13 @@ os.chdir(os.path.join(root, "python-chebifier"))
 predict(
     ensemble_config=os.path.join(root, "..", "..", "checkpoints", "ensemble_config.yml"),
     smiles=(),  # none inline
-    smiles_file=os.path.join(root, "..", input_file),
+    smiles_file=os.path.join(root, "..", tmp_file),
     output=os.path.join(root, "..", output_file.replace(".csv", ".json")),
     ensemble_type="wmv-f1",
     chebi_version=241,
     use_confidence=True,
     resolve_inconsistencies=True
 )
-
 
 # # run model
 # cmd = [
@@ -39,20 +52,22 @@ predict(
 #     ]
 # subprocess.run(cmd, check=True, cwd=os.path.join(root, "python-chebifier"))
 
-# read input smiles from .csv file
-smiles = [i.strip() for i in open(os.path.join(root, "..", input_file), "r").readlines()[1:]]
 
 # read json output
 output = json.load(open(os.path.join(root, "..", output_file.replace(".csv", ".json"))))
 output_content = ['chebi_predicted_parents']
-for smi in smiles:
+for smi in smiles_list:
     r = ["CHEBI:" + o for o in sorted(output[smi])]
     r = ";".join(r)
     output_content.append(r)
 
 # remove json output
 os.remove(os.path.join(root, "..", output_file.replace(".csv", ".json")))
+os.remove(os.path.join(root, "..", tmp_file))
 
 # write output in a .csv file
-with open(os.path.join(root, "..", output_file), "w") as f:
-    f.write("\n".join(output_content))
+csv_path = os.path.join(root, "..", output_file)
+with open(csv_path, "w", newline="") as f:
+    writer = csv.writer(f)
+    for row in output_content:
+        writer.writerow([row])
